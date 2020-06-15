083cb223dbadbb37ec59ffa87452bed3c87922de
Samory Diaby <43681215+samdiaby@users.noreply.github.com>
2020-06-15 19:35:10 +0200

    add covidata2019 project

---
+++ Covidata2019/app/src/main/java/com/epita/android/covidata2019/Data.kt
@@ -0,0 +1,13 @@
+package com.epita.android.covidata2019
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Data : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data)
+    }
+}
